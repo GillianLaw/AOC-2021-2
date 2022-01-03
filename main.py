5b2874260ef70d559b07ea80aef571d075eb2b13
AriandeL <79564944+GillianLaw@users.noreply.github.com>
2022-01-03 19:13:07 +0000

    Tidied up

--- main.py
+++ main.py
@@ -1,32 +1,13 @@
 import time
 
-"""
-Thanks for using Replit for Advent of Code!
 
-Here are a few tips:
-
-1. To install packages, just import them and Replit will install them for you, or click on the cube in the sidebar to install manually.
-2. If you're stuck, try using the debugger in the sidebar shaped like a play/pause button.
-3. When you're done, you can share your project by clicking the project name and then "Publish"
-3.a When you share your project, use the #adventofcode hashtag!
-4. Have fun, and good luck!
-"""
-
-
-"""
-Place your question data into the data.txt file.
-You may need to parse the data!
-"""
 with open('data.txt') as f:
     change = [line.rstrip() for line in f]
 
 
 if __name__ == "__main__":
   replit_start_time = time.perf_counter()
-  """
-  Run your functions here.
-  This code won't be run if imported.
-  """
+ 
 
 distance = 0
 deep = 0
@@ -59,7 +40,7 @@ print((deep - aim) * distance)
 
 
 
-  # Keep this line at the end of your code
+ 
   
 replit_end_time = time.perf_counter()
 print("Elapsed time:", replit_end_time - replit_start_time)
